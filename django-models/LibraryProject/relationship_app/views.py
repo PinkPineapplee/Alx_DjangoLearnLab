697b2dda81ca810f2c97c5272bf342b1c7128c03
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-11-22 12:10:12 +0100

    update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Library, Book
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 
 
