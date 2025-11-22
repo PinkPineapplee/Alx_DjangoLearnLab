f88c4ec8816fd96511f0880e97b6a3d1e83fcda4
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-11-22 11:30:51 +0100

    update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
 from .models import Library, Book
+from django.views.generic.detail import DetailView
 
 
 # Function-based view: list all books
