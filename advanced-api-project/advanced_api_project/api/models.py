b79649c5ef1dd67ce91ad2d5a974f46f860bab58
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-12-02 10:35:31 +0100

    create book and author model

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length = 150)
+    author = models.ForeignKey(Author) 
+    release_date = models.DateField()
+    borrowed_date = models.DateField()   
