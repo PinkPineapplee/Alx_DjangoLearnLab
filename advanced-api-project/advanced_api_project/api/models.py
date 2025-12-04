c89d8e0512ef223f0119d46c1a5449071c6bfbe3
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-12-04 21:27:51 +0100

    Add serlizer.py

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length = 200)
+    books = models.ForeignKey(Book)
 
-
-
+    def __str__(self):
+       return self.name
 class Book(models.Model):
-    title = models.CharField(max_length = 150)
-    author = models.ForeignKey(Author) 
+    title = models.CharField(max_length = 250)
+    author = models.ForeignKey(Author, on_delete =models.CASCADE, related_name = 'books') 
     publication_year = models.DateField()
     borrowed_date = models.DateField()   
+
+    def __str__(self):
+        return self.title
