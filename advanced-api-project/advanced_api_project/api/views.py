f0bb53deb8659919712c8914583cbf614659ed68
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-12-10 09:38:45 +0100

    booklist view created

--- advanced-api-project/advanced_api_project/api/views.py
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+
 
 # Create your views here.
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
