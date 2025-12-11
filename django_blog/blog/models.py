3db3ece6b725ae6f56557628f565b637070f86cb
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-12-11 19:58:49 +0100

    fix buggy templates

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -35,3 +35,29 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
     else:
         instance.profile.save()
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']  # oldest first; change to '-created_at' for newest first
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
+
+    def get_edit_url(self):
+        return reverse('comment-update', kwargs={'pk': self.pk})
+
+    def get_delete_url(self):
+        return reverse('comment-delete', kwargs={'pk': self.pk})
+
+
+
+
+
